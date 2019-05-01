0f3b113bab7d6d658bf063b0c10ef25b5c4c0341
SuslikV <vx2010@i.ua>
2019-05-01 12:22:22 +0300

    First try to make let's see what is missing

--- CI/build-win-av.cmd
+++ CI/build-win-av.cmd
@@ -1,2 +1,28 @@
 REM Build script for appveyor (Windows)
-echo Building started...
+
+REM echo status
+echo
+REM Some space
+echo:
+
+REM Let us see what we have as environment here
+echo %PATH%
+echo:
+cd 
+
+REM cmake will unable to compile without "MinGW\bin" path to PATH
+set PATH=C:\MinGW\bin;%PATH%
+echo:
+echo PATH environment variable was updated.
+echo %PATH%
+echo:
+
+REM Make new building dir
+mkdir %APPVEYOR_BUILD_FOLDER%\build
+cd %APPVEYOR_BUILD_FOLDER%\build
+
+cmake --version
+cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DUSE_CXX_GNU_EXTENSIONS:BOOL=ON ..
+REM mingw32-make --version
+REM mingw32-make VERBOSE=1
+REM mingw32-make install
