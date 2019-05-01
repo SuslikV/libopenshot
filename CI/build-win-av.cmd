2d66d27978acdc5f4d6d101e6f492c8e8cf1ff2c
SuslikV <vx2010@i.ua>
2019-05-01 22:13:04 +0300

    Let's build libopenshot

--- CI/build-win-av.cmd
+++ CI/build-win-av.cmd
@@ -23,6 +23,6 @@ cd %APPVEYOR_BUILD_FOLDER%\build
 
 cmake --version
 cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DUSE_CXX_GNU_EXTENSIONS:BOOL=ON ..
-REM mingw32-make --version
-REM mingw32-make VERBOSE=1
+mingw32-make --version
+mingw32-make VERBOSE=1
 REM mingw32-make install
