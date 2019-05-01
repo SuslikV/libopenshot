6445dfbc4629912d34f3a453e4d5c0112fa82584
SuslikV <vx2010@i.ua>
2019-05-01 12:58:45 +0300

    Add FFmpeg dependency libs

--- CI/install-win-av.cmd
+++ CI/install-win-av.cmd
@@ -17,5 +17,17 @@ set PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%
 REM let us see what is installed within MSYS2
 bash -lc "pacman -Q"
 
+REM Create downloads folder for external dependencies
+IF NOT EXIST "%APPVEYOR_BUILD_FOLDER%\downloads" mkdir %APPVEYOR_BUILD_FOLDER%\downloads
+
+REM Download FFmpeg dependencies
+cd %APPVEYOR_BUILD_FOLDER%\downloads
+IF NOT EXIST "ffmpeg-20190429-ac551c5-win64-dev.zip" curl -kLO https://ffmpeg.zeranoe.com/builds/win64/dev/ffmpeg-20190429-ac551c5-win64-dev.zip -f --retry 4
+dir
+REM 7z x ffmpeg-20190429-ac551c5-win64-dev.zip -offmpeg
+dir /s
+REM Add ffmpeg folders to PATH
+set FFMPEGDIR=%APPVEYOR_BUILD_FOLDER%\downloads\ffmpeg
+
 REM unmute output
 @echo on
