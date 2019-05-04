e6c48a329e294354b53a0e5edea0a87eec81a706
SuslikV <vx2010@i.ua>
2019-05-04 09:34:20 +0300

    Remove Python2

--- CI/install-win-av.cmd
+++ CI/install-win-av.cmd
@@ -20,8 +20,10 @@ set PATH=C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\bin;%PATH%
 REM let us see what is installed within MSYS2
 bash -lc "pacman -Q"
 
-REM look for python 3.6
-bash -lc "pacman -Ss python3"
+REM Remove python2
+bash -lc "pacman -Rs --noconfirm python2"
+REM Remove python2 from PATH
+ren "C:\Python27" "C:\Python2_7"
 
 exit 1
 
