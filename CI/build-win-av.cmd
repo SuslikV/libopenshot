b6aab91eff584658892e8a3ad18b6bf5ce4e3a22
SuslikV <vx2010@i.ua>
2019-05-09 00:14:03 +0300

    Code formatting

--- CI/build-win-av.cmd
+++ CI/build-win-av.cmd
@@ -1,32 +1,18 @@
 REM Build script for appveyor (Windows)
 
 REM echo status
-echo
+ECHO
 REM Some space
-echo:
-
-REM Let us see what we have as environment here
-echo %PATH%
-echo:
-cd 
-
-REM cmake will unable to compile without "MinGW\bin" path to PATH
-REM set PATH=C:\MinGW\bin;%PATH%
-REM set PATH=C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\bin;%PATH%
-REM echo:
-REM echo PATH environment variable was updated.
-REM echo %PATH%
-REM echo:
+ECHO:
 
 REM Make new building dir
-mkdir %APPVEYOR_BUILD_FOLDER%\build
-cd %APPVEYOR_BUILD_FOLDER%\build
+MKDIR %APPVEYOR_BUILD_FOLDER%\build
+CD %APPVEYOR_BUILD_FOLDER%\build
 
 cmake --version
 cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DPYTHON_EXECUTABLE="C:/Python36-x64/python.exe" -DPYTHON_INCLUDE_DIR="C:/Python36-x64/include/" -DPYTHON_LIBRARY="C:/Python36-x64/libs/libpython36.a" -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_SYSTEM_VERSION=6.1 ..
 mingw32-make --version
 mingw32-make VERBOSE=1
-REM mingw32-make test
 REM Look for shared lib
-dir /s libopenshot.dll
+DIR /s libopenshot.dll
 mingw32-make install
